6c4a2e1f362c6b748a940f4c248330eb60251801
Louis Beal <louis.j.beal@gmail.com>
2021-12-16 11:00:34 +0100

    commit before reworking coverage to end-only run

--- docs/docs_source/conf.py
+++ docs/docs_source/conf.py
@@ -10,8 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import inspect
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('../..'))
 
 
@@ -29,7 +31,6 @@ author = 'LBeal'
 # ones.
 extensions = ['sphinx_rtd_theme',
               'sphinx.ext.napoleon',
-              'sphinx.ext.coverage',
               'sphinx.ext.autodoc',
               ]
 
@@ -46,30 +47,6 @@ coverage_ignore_pyobjects = ['test_*']
 
 autodoc_member_order = 'bysource'
 
-
-def autodoc_skip_member_handler(app, what, name, obj, skip, options):
-    # start patterns to exclude from autodoc
-    exclude_startswith = ['__',  # catch dunder functions
-                          'test_',  # ignore any tests
-                          ]
-
-    skip_start = any(name.startswith(pattern)
-                     for pattern in exclude_startswith)
-
-    # seems to still be documenting functions without docstrings,
-    # exclude those here
-    empty_doc = True
-    if hasattr(obj, '__doc__'):
-        empty_doc = False
-        docstr = obj.__doc__
-
-        if docstr is None:
-            empty_doc = True
-        if docstr == '':
-            empty_doc = True
-
-    skip = skip_start or empty_doc
-    return skip
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -88,5 +65,141 @@ html_static_path = ['../_static']
 # sphinx-build -E -a -b coverage ./docs/docs_source/ ./docs/
 
 
+class Coverage:
+    """
+    handler for code coverage to replace shortcomings with sphinx's coverage
+
+
+    Add to conf.py:
+
+
+    def setup(app):
+        coverage_handler = Coverage()
+        app.connect('autodoc-skip-member', coverage_handler.autodoc_skip)
+        app.connect('source-read', coverage_handler.source_read)
+        app.connect('build-finished', coverage_handler.finish)
+
+    """
+
+    def __init__(self, logfile='coverage.txt'):
+
+        self._logpath = self._create_log(logfile)
+        self._sections = {}
+
+        self._count = 0  # counter for total modules considered
+
+        self._current_doc = ''
+
+        self._tree = {}
+
+    def autodoc_skip(self, app, what, name, obj, skip, options):
+
+        # start patterns to exclude from autodoc
+        exclude_startswith = ['__',  # catch dunder functions
+                              'test_',  # ignore any tests
+                              ]
+
+        skip_start = any(name.startswith(pattern)
+                         for pattern in exclude_startswith)
+
+        if skip_start:
+            # no point checking for docstring if we're skipping anyway
+            return True
+
+        self._count += 1
+
+        # manage undocumented sections
+        empty_doc = True
+        if hasattr(obj, '__doc__'):
+            empty_doc = False
+            docstr = obj.__doc__
+
+            if docstr is None:
+                empty_doc = True
+            if docstr == '':
+                empty_doc = True
+
+        print(name)
+        if empty_doc and not skip_start:
+            self._update_log(name)
+
+        return empty_doc
+
+    def _update_tree(self, pydomain):
+
+        for obj in pydomain['objects']:
+            leafpath = obj.split('.')
+            print(leafpath)
+
+            # self._tree[leafpath[0]] = self._make_branch(leafpath)
+
+
+    def _make_branch(self, leafpath):
+        print('adding', leafpath[0])
+        stub = {}
+
+        stub[leafpath[0]] = self._make_branch([leafpath[1:]])
+
+        return stub
+
+    def source_read(self, app, docname, source):
+        # reading a new source file, create a section
+
+        print('#'*24)
+        for item in app.env.domains['py'].get_objects():
+            print(item)
+
+        end = os.path.split(docname)[-1]
+
+        self._current_doc = end
+
+        end = os.path.split(docname)[-1]
+
+        self._sections[end] = []
+
+    def _create_log(self, logname):
+        import os
+        logpath = os.path.abspath(logname)
+        print('coverage log will be created at ' + logpath)
+
+        return logpath
+
+    def _update_log(self, name):
+
+        self._sections[self._current_doc].append(name)
+
+    def finish(self, app, exception):
+        """Connect to sphinx event `build-finished(app, exception)`"""
+
+        initial_log_line = 'Undocumented Objects:'
+
+        print('Coverage analysis complete, writing log at ' +
+              self._logpath)
+
+        undoc_count = 0
+
+        with open(self._logpath, 'w+') as o:
+            o.write(initial_log_line+'\n')
+            o.write('='*len(initial_log_line) + '\n\n')
+
+            for doc, section in self._sections.items():
+                if len(section) > 0:
+                    o.write(doc + '\n')
+                    o.write('-'*len(doc) + '\n')
+                    for line in section:
+                        o.write(f'- {line}\n')
+                        undoc_count += 1
+                    o.write('\n')
+
+            undoc_pc = 100 - (100 * undoc_count / self._count)
+            o.write('\nCoverage')
+            o.write('\n'+'-'*8)
+            o.write(f'\n{undoc_count} undocumented of {self._count} objects')
+            o.write(f'\n==> {undoc_pc:.2f}% coverage')
+
+
 def setup(app):
-    app.connect('autodoc-skip-member', autodoc_skip_member_handler)
+    coverage_handler = Coverage()
+    app.connect('autodoc-skip-member', coverage_handler.autodoc_skip)
+    app.connect('source-read', coverage_handler.source_read)
+    app.connect('build-finished', coverage_handler.finish)
