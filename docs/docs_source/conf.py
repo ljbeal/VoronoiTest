f5a6c876c53217d1e2827660ea905831b3e13805
Louis Beal <louis.j.beal@gmail.com>
2022-01-04 13:36:03 +0100

    merge changes from bigdft

--- docs/docs_source/conf.py
+++ docs/docs_source/conf.py
@@ -10,12 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import inspect
-import os
+import os.path as p
 import sys
+import types
+import inspect
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, p.abspath('../..'))
 
+DEBUG = True
 
 # -- Project information -----------------------------------------------------
 
@@ -69,44 +71,76 @@ class Coverage:
     """
     handler for code coverage to replace shortcomings with sphinx's coverage
 
-
     Add to conf.py:
 
-
     def setup(app):
-        coverage_handler = Coverage()
-        app.connect('autodoc-skip-member', coverage_handler.autodoc_skip)
-        app.connect('source-read', coverage_handler.source_read)
-        app.connect('build-finished', coverage_handler.finish)
+        Coverage(app)
 
+    Args:
+        app:
+            sphinx-build app, for connections
     """
 
-    def __init__(self, logfile='coverage.txt'):
+    DEBUG = False
 
+    def __init__(self, app, logfile='coverage.txt'):
+
+        # initialise connections with the sphinx builder
+        self._connect_funcs(app)
+
+        # logfile to output coverage stats to
         self._logpath = self._create_log(logfile)
-        self._sections = {}
+        self._autodoc = False  # don't output if there's no autodoc
 
-        self._count = 0  # counter for total modules considered
+        # custom counted object list
+        self.objects = []
 
-        self._current_doc = ''
+        # section tracking
+        self._current_section = ''
+        # self._current__file__ = ''
+        self._current__load__ = ''
+        self._sourcefile = ''
 
-        self._tree = {}
+        # exclude patterns
+        self.exclude_startswith = ['__',  # catch dunder functions
+                                   'test_',  # ignore any tests
+                                   '_',  # private members
+                                  ]
+
+    def _connect_funcs(self, app):
+        app.connect('autodoc-skip-member', self.autodoc_skip)
+        app.connect('source-read', self.source_read)
+        app.connect('build-finished', self.finish)
+        app.connect('autodoc-process-signature', self.obj_prepro)
+
+    def _eliminate_by_name(self, name):
+        """eliminate objects based on their name"""
+
+        skip = any(name.startswith(pattern) for pattern in
+                   self.exclude_startswith)
+
+        return skip
 
     def autodoc_skip(self, app, what, name, obj, skip, options):
+        """Determine whether to skip this object
 
-        # start patterns to exclude from autodoc
-        exclude_startswith = ['__',  # catch dunder functions
-                              'test_',  # ignore any tests
-                              ]
+        Returns:
+            (bool) skip:
+                skip flag passed back to autodoc
+        """
+        self._autodoc = True
 
-        skip_start = any(name.startswith(pattern)
-                         for pattern in exclude_startswith)
-
-        if skip_start:
+        if self._eliminate_by_name(name):
             # no point checking for docstring if we're skipping anyway
             return True
 
-        self._count += 1
+        # need to know if we're at the start of the module
+        mode_module = False
+        if self._current_section == self._sourcefile:
+            mode_module = True
+
+        if self.DEBUG:
+            print('\t', name, what, type(obj), end=' ')
 
         # manage undocumented sections
         empty_doc = True
@@ -119,43 +153,14 @@ class Coverage:
             if docstr == '':
                 empty_doc = True
 
-        print(name)
-        if empty_doc and not skip_start:
-            self._update_log(name)
+        if empty_doc:
+            if self.DEBUG:
+                print(f'no doc for {name}')
+
+        elif self.DEBUG:
+            print()
 
         return empty_doc
-
-    def _update_tree(self, pydomain):
-
-        for obj in pydomain['objects']:
-            leafpath = obj.split('.')
-            print(leafpath)
-
-            # self._tree[leafpath[0]] = self._make_branch(leafpath)
-
-
-    def _make_branch(self, leafpath):
-        print('adding', leafpath[0])
-        stub = {}
-
-        stub[leafpath[0]] = self._make_branch([leafpath[1:]])
-
-        return stub
-
-    def source_read(self, app, docname, source):
-        # reading a new source file, create a section
-
-        print('#'*24)
-        for item in app.env.domains['py'].get_objects():
-            print(item)
-
-        end = os.path.split(docname)[-1]
-
-        self._current_doc = end
-
-        end = os.path.split(docname)[-1]
-
-        self._sections[end] = []
 
     def _create_log(self, logname):
         import os
@@ -164,42 +169,146 @@ class Coverage:
 
         return logpath
 
-    def _update_log(self, name):
-
-        self._sections[self._current_doc].append(name)
-
     def finish(self, app, exception):
-        """Connect to sphinx event `build-finished(app, exception)`"""
+        """Connect to sphinx event `build-finished(app, exception)`
+
+        Final cleanup, calculation and output of coverage stats
+        """
+
+        if not self._autodoc:
+            print('coverage not outputting: autodoc was never called'
+                  '(use apidoc to create sourcefiles)')
+            return None
 
         initial_log_line = 'Undocumented Objects:'
 
         print('Coverage analysis complete, writing log at ' +
               self._logpath)
 
-        undoc_count = 0
+        sphinx_objects = app.env.domaindata['py']['objects'].keys()
+        undoc = sorted(list(set(self.objects) - set(sphinx_objects)))
+        undoc_pc = 100 - 100*len(undoc)/len(self.objects)
+
+        if self.DEBUG:
+            print('#'*24 + ' Documented objects:')
+            print(sphinx_objects)
+            print('#'*24 + ' Objects found in source:')
+            print(self.objects)
+            print('#'*24 + ' Undocumented objects:')
+            print(undoc)
+            print('#'*24 + ' Coverage percentage:')
+            print(f'{undoc_pc:.2f}')
 
         with open(self._logpath, 'w+') as o:
+
             o.write(initial_log_line+'\n')
-            o.write('='*len(initial_log_line) + '\n\n')
+            o.write('='*len(initial_log_line)+'\n')
 
-            for doc, section in self._sections.items():
-                if len(section) > 0:
-                    o.write(doc + '\n')
-                    o.write('-'*len(doc) + '\n')
-                    for line in section:
-                        o.write(f'- {line}\n')
-                        undoc_count += 1
-                    o.write('\n')
+            for name in undoc:
+                o.write('\n'+name)
 
-            undoc_pc = 100 - (100 * undoc_count / self._count)
-            o.write('\nCoverage')
-            o.write('\n'+'-'*8)
-            o.write(f'\n{undoc_count} undocumented of {self._count} objects')
-            o.write(f'\n==> {undoc_pc:.2f}% coverage')
+            o.write('\n\n')
+            o.write('Coverage percentage:\n')
+            o.write('--------------------\n')
+            o.write(f'{undoc_pc:.2f}% of discovered objects documented\n')
+            o.write(f'{len(undoc)} items are missing docstrings')
+
+    def source_read(self, app, docname, source):
+        """Hook into sphinx source read function"""
+        name = docname.split('/')[-1]
+        self._sourcefile = name
+        if self.DEBUG:
+            print()
+            print('new sourcefile', name)
+
+    def _count_object(self, objname, obj, rootname, depth=0):
+        """recursively count store for comparison
+
+        Args:
+            objname (str):
+                name of the object
+            obj (object):
+                object itself (for recursion and other checks)
+            rootname (str):
+                call path up until now
+            depth:
+                tab indentation for debugging recursion
+        returns:
+            None
+        """
+        appname = rootname + '.' + objname
+        if self.DEBUG:
+            print('\t'*depth + f'considering {appname}', end='... ')
+        msg = []
+
+        skip = False
+
+        # remove same names as autodoc
+        if self._eliminate_by_name(objname):
+            skip = True
+            msg.append('blacklisted name, ')
+
+        # avoids most imports
+        # if hasattr(obj, '__file__'):
+        #     if obj.__file__ != self._current__file__:
+        #         skip = True
+        #         msg.append('external import, ')
+
+        if hasattr(obj, '__loader__'):
+            if obj.__loader__ != self._current__load__:
+                skip = True
+                msg.append('loader mismatch (external import?), ')
+
+        # skip uncallables (but NOT properties!)
+        if not hasattr(obj, '__call__'):
+            if not hasattr(obj, '__set__'):
+                skip = True
+                msg.append('non callable structure, ')
+
+        if hasattr(obj, '__module__'):
+            if obj.__module__ != self._current_section:
+                skip = True
+                msg.append('module mismatch (local import from '
+                           f'{obj.__module__}), ')
+
+            elif not skip:
+                # also recursively grab members, if we're not already skipping
+                for membername, member in vars(obj).items():
+                    self._count_object(membername, member,
+                                       appname, depth+1)
+
+        # specific debugging
+        # if objname in ['TEST', 'endpoint']:
+        #     print('\nspecified debugging for ', objname)
+        #     print(dir(obj))
+
+        if not skip:
+            self.objects.append(appname)
+            if self.DEBUG:
+                print('appended as', appname)
+        elif self.DEBUG:
+            print(''.join(msg)[:-2] + '. Skipped.')
+
+    def obj_prepro(self, app, what, name, obj, options, signature, return_annotation):
+        """Hook into sphinx object call and store the object members"""
+        if self.DEBUG:
+            print('new section,', name)
+        self._current_section = name
+
+        # self._current__file__ = getattr(obj, '__file__', '')
+        self._current__load__ = getattr(obj, '__loader__', '')
+
+        modname = getattr(obj, '__module__', None)
+        # loader = getattr(obj, '__loader__', None)
+
+        # if this object is a module, get its members for comparison
+        if isinstance(obj, types.ModuleType):
+            if self.DEBUG:
+                print('moduletype', name, modname)
+
+            for membername, member in vars(obj).items():
+                self._count_object(membername, member, name, depth=1)
 
 
 def setup(app):
-    coverage_handler = Coverage()
-    app.connect('autodoc-skip-member', coverage_handler.autodoc_skip)
-    app.connect('source-read', coverage_handler.source_read)
-    app.connect('build-finished', coverage_handler.finish)
+    coverage_handler = Coverage(app)
